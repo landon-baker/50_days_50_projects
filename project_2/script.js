7b84f905aa52af74bdd978dde33a7271cf52f15e
Landon Baker <landon.baker@utexas.edu>
2021-01-24 13:20:43 -0600

    complete project 2

---
+++ project_2/script.js
@@ -0,0 +1,36 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+  if (currentActive !== circles.length) currentActive++
+  update()
+})
+
+prev.addEventListener('click', () => {
+  if (currentActive !== 1) currentActive--
+  update()
+})
+
+const update = () => {
+  circles.forEach((circle, ind) =>
+    ind < currentActive
+      ? circle.classList.add('active')
+      : circle.classList.remove('active')
+  )
+
+  const actives = document.querySelectorAll('.active')
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
+
+  if (currentActive === 1) prev.disabled = true
+  else if (currentActive === circles.length) next.disabled = true
+  else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
